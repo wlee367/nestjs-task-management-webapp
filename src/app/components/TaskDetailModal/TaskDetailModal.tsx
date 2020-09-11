4c24974c27bccace96eb8202a6a2e02d94250ae9
Won Ryul Lee <wlee367@uwo.ca>
2020-09-11 13:46:56 -0400

    Basic Styling for comments.

--- src/app/components/TaskDetailModal/TaskDetailModal.tsx
+++ src/app/components/TaskDetailModal/TaskDetailModal.tsx
@@ -1,39 +1,40 @@
-import React from "react";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import { useHistory, useParams } from "react-router-dom";
-import CloseIcon from "@material-ui/icons/Close";
-import Styled from "styled-components";
-import ListIcon from "@material-ui/icons/List";
-import { makeStyles, Theme } from "@material-ui/core/styles";
-import AssignmentIcon from "@material-ui/icons/Assignment";
-import StatusToggler from "../StatusToggler/StatusToggler";
-import { TaskStatus } from "../StatusToggler/StatusEnums";
-import { CommentInput } from "../CommentInput/CommentInput";
-import { Comment } from "../../redux/actions/index";
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { useHistory, useParams } from 'react-router-dom';
+import CloseIcon from '@material-ui/icons/Close';
+import Styled from 'styled-components';
+import ListIcon from '@material-ui/icons/List';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import StatusToggler from '../StatusToggler/StatusToggler';
+import { TaskStatus } from '../StatusToggler/StatusEnums';
+import { CommentInput } from '../CommentInput/CommentInput';
+import { Comment } from '../../redux/actions/index';
+import moment from 'moment';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  dialogPaper: {
-    minHeight: "80vh",
-    maxHeight: "80vh",
-  },
+    dialogPaper: {
+        minHeight: '80vh',
+        maxHeight: '80vh',
+    },
 
-  dialogContent: {
-    display: "flex",
-    flexDirection: "row",
-  },
+    dialogContent: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
 }));
 
 type TaskDetailModalProps = {
-  toggleModal: () => void;
-  isOpen: boolean;
-  title: string;
-  content: string;
-  selectedStatus: string;
-  comments: [];
-  onStatusChange: (newStatus: string) => void;
-  onSubmitComment: (taskId: string, commentText: string) => void;
+    toggleModal: () => void;
+    isOpen: boolean;
+    title: string;
+    content: string;
+    selectedStatus: string;
+    comments: [];
+    onStatusChange: (newStatus: string) => void;
+    onSubmitComment: (taskId: string, commentText: string) => void;
 };
 
 const StyledDialogTitleDiv = Styled.div`
@@ -87,132 +88,164 @@ const StyledTitleHeader = Styled.div`
 `;
 
 const CommentCard = Styled.div`
-    padding: 1em 0;
+    padding: 0.5em 0.5em;
 
     display: flex;
     flex-direction: row;
     align-items: center;
+
+    border: 1px solid grey;
+    border-radius: 8px;
+
+    &> .comment {
+      padding-left: 0.25em;
+    }
+
+    &>.createdAt {
+      padding-left: 0.25em;
+    }
 `;
 
 interface TaskDetailModalParam {
-  id: string;
+    id: string;
 }
 
 export const TaskDetailModal = (props: TaskDetailModalProps) => {
-  const { isOpen, toggleModal, title, content, selectedStatus } = props;
+    const { isOpen, toggleModal, title, content, selectedStatus } = props;
 
-  const history = useHistory();
+    const history = useHistory();
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const params: TaskDetailModalParam = useParams();
+    const params: TaskDetailModalParam = useParams();
 
-  const handleClose = () => {
-    toggleModal();
-    history.goBack();
-  };
+    const handleClose = () => {
+        toggleModal();
+        history.goBack();
+    };
 
-  /**
-   * @param status
-   */
-  const determineSelectedStatusEnum = (status: string) => {
-    switch (status) {
-      case "OPEN":
-        return TaskStatus.OPEN;
-      case "IN_PROGRESS":
-        return TaskStatus.IN_PROGRESS;
-      case "DONE":
-        return TaskStatus.DONE;
-      default:
-        return TaskStatus.OPEN;
-    }
-  };
+    /**
+     * @param status
+     */
+    const determineSelectedStatusEnum = (status: string) => {
+        switch (status) {
+            case 'OPEN':
+                return TaskStatus.OPEN;
+            case 'IN_PROGRESS':
+                return TaskStatus.IN_PROGRESS;
+            case 'DONE':
+                return TaskStatus.DONE;
+            default:
+                return TaskStatus.OPEN;
+        }
+    };
 
-  const handleStatusOnChange = (newStatus: string) => {
-    props.onStatusChange(newStatus);
-  };
+    const handleStatusOnChange = (newStatus: string) => {
+        props.onStatusChange(newStatus);
+    };
 
-  const handleSubmitComment = (comment: string) => {
-    let { id } = params;
+    const handleSubmitComment = (comment: string) => {
+        let { id } = params;
 
-    props.onSubmitComment(id, comment);
-  };
+        props.onSubmitComment(id, comment);
+    };
 
-  return (
-    <div>
-      <Dialog
-        classes={{ paper: classes.dialogPaper }}
-        disableBackdropClick={true}
-        fullWidth={true}
-        maxWidth="md"
-        open={isOpen}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle
-          style={{ paddingTop: "1.5em", alignItems: "center" }}
-          id="form-dialog-title"
-        >
-          <StyledDialogTitleDiv>
-            <div
-              style={{ alignItems: "center", display: "flex", width: "100%" }}
+    return (
+        <div>
+            <Dialog
+                classes={{ paper: classes.dialogPaper }}
+                disableBackdropClick={true}
+                fullWidth={true}
+                maxWidth="md"
+                open={isOpen}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title"
             >
-              <AssignmentIcon />
-              <div style={{ marginLeft: "0.5em", width: "100%" }}>{title}</div>
-            </div>
-            <StyledCloseIcon onClick={handleClose} />
-          </StyledDialogTitleDiv>
-        </DialogTitle>
-        <DialogContent
-          classes={{ root: classes.dialogContent }}
-          style={{ paddingTop: 0, paddingBottom: "1.5em" }}
-        >
-          <StyledContentContainer>
-            <StyledDetailHeader>
-              <StyledTitleHeader>
-                <ListIcon />
-                <h3 style={{ marginLeft: "0.5em" }}>Details: </h3>
-              </StyledTitleHeader>
-              <StyledDetailContent>
-                <p>{content}</p>
-              </StyledDetailContent>
-            </StyledDetailHeader>
-            <StyledCommentsContainer>
-              <StyledCommentsHeader>
-                {" "}
-                <h3 style={{ marginLeft: "0.5em" }}>Comments: </h3>
-              </StyledCommentsHeader>
-              <StyledCommentInputContainer>
-                <CommentInput
-                  userInitials={"JL"}
-                  submitComment={(comment: string) => {
-                    handleSubmitComment(comment);
-                  }}
-                />
-              </StyledCommentInputContainer>
-              {props.comments &&
-                props.comments.map((comment: Comment) => {
-                  console.log(comment);
-                  return (
-                    <CommentCard key={comment.id}>
-                      <div>{comment.user.username}</div>
-                      {comment.commentText}
-                      <div>{comment.createdAt}</div>
-                    </CommentCard>
-                  );
-                })}
-            </StyledCommentsContainer>
-          </StyledContentContainer>
-          <StyledWidgetContainer>
-            <StatusToggler
-              status={determineSelectedStatusEnum(selectedStatus)}
-              changeStatus={(newStatus: string) => {
-                handleStatusOnChange(newStatus);
-              }}
-            />
-          </StyledWidgetContainer>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
+                <DialogTitle
+                    style={{ paddingTop: '1.5em', alignItems: 'center' }}
+                    id="form-dialog-title"
+                >
+                    <StyledDialogTitleDiv>
+                        <div
+                            style={{
+                                alignItems: 'center',
+                                display: 'flex',
+                                width: '100%',
+                            }}
+                        >
+                            <AssignmentIcon />
+                            <div style={{ marginLeft: '0.5em', width: '100%' }}>
+                                {title}
+                            </div>
+                        </div>
+                        <StyledCloseIcon onClick={handleClose} />
+                    </StyledDialogTitleDiv>
+                </DialogTitle>
+                <DialogContent
+                    classes={{ root: classes.dialogContent }}
+                    style={{ paddingTop: 0, paddingBottom: '1.5em' }}
+                >
+                    <StyledContentContainer>
+                        <StyledDetailHeader>
+                            <StyledTitleHeader>
+                                <ListIcon />
+                                <h3 style={{ marginLeft: '0.5em' }}>
+                                    Details:{' '}
+                                </h3>
+                            </StyledTitleHeader>
+                            <StyledDetailContent>
+                                <p>{content}</p>
+                            </StyledDetailContent>
+                        </StyledDetailHeader>
+                        <StyledCommentsContainer>
+                            <StyledCommentsHeader>
+                                {' '}
+                                <h3 style={{ marginLeft: '0.5em' }}>
+                                    Comments:{' '}
+                                </h3>
+                            </StyledCommentsHeader>
+                            <StyledCommentInputContainer>
+                                <CommentInput
+                                    userInitials={'JL'}
+                                    submitComment={(comment: string) => {
+                                        handleSubmitComment(comment);
+                                    }}
+                                />
+                            </StyledCommentInputContainer>
+                            {props.comments &&
+                                props.comments.map((comment: Comment) => {
+                                    console.log(comment);
+                                    return (
+                                        <CommentCard key={comment.id}>
+                                            <p className="username">
+                                                <b>{comment.user.username}</b>{' '}
+                                                commented:{' '}
+                                            </p>
+                                            <p className="comment">
+                                                "{comment.commentText}"
+                                            </p>
+                                            <p className="createdAt">
+                                                at:{' '}
+                                                {moment(
+                                                    comment.createdAt,
+                                                    'YYYY-MM-DD HH:mm:ss'
+                                                ).format('LTS')}
+                                            </p>
+                                        </CommentCard>
+                                    );
+                                })}
+                        </StyledCommentsContainer>
+                    </StyledContentContainer>
+                    <StyledWidgetContainer>
+                        <StatusToggler
+                            status={determineSelectedStatusEnum(selectedStatus)}
+                            changeStatus={(newStatus: string) => {
+                                handleStatusOnChange(newStatus);
+                            }}
+                        />
+                    </StyledWidgetContainer>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
 };
